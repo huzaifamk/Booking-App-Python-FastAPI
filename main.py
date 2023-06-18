0e86d30d1126dbcf5939f1363f91710a396a8c54
huzaifamk <malikhuzaifa99@gmail.com>
2023-06-18 20:28:06 +0500

    Added class models

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+class Booking(BaseModel):
+    Hotel_Name: str
+    Address: str
+    Price: int
+    Is_Available: bool
+    Rating : Optional[float] = None
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/bookings")
 async def bookings():
     return {"message": "Bookings"}
 
+
 @app.post("/create-booking")
-async def create_booking(payload: dict = Body(...)):
+async def create_booking(payload: Booking):
     print(payload)
-    return {"Title": f"Booking for {payload['Hotel_Name']} created successfully"}
+    return {"Title": f"Booking for {payload.Hotel_Name} is created successfully with price {payload.Price} and address {payload.Address} and rating {payload.Rating}"}
